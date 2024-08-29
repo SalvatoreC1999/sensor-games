9b0d1538b5f89df32ba1f5b85002a823b2e1c1a0
Salvatore <sal.contessa@gmail.com>
2024-08-29 12:15:07 +0200

    added lottie

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,9 @@
-import {ApplicationConfig, ApplicationRef, Injector, provideZoneChangeDetection} from '@angular/core';
+import {
+  ApplicationConfig,
+  ApplicationRef,
+  Injector,
+  provideZoneChangeDetection
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
@@ -6,9 +11,19 @@ import {provideHttpClient} from "@angular/common/http";
 import {provideAnimations} from "@angular/platform-browser/animations";
 import {DialogService} from "primeng/dynamicdialog";
 import {DOCUMENT} from "@angular/common";
+import {provideLottieOptions} from "ngx-lottie";
+import player from 'lottie-web';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes),provideHttpClient(),provideAnimations(),    {
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideHttpClient(),
+    provideAnimations(),
+    provideLottieOptions({
+      player: () => player,
+    }),
+    {
     provide: DialogService,
     useFactory: (appRef: ApplicationRef, injector: Injector, document: Document) => {
       return new DialogService(appRef, injector, document);
--- src/app/dodge-the-obstacle/dodge-the-obstacle.component.ts
+++ src/app/dodge-the-obstacle/dodge-the-obstacle.component.ts
@@ -5,13 +5,8 @@ import {Button} from "primeng/button";
 import {NgClass, NgForOf, NgIf} from "@angular/common";
 import {GameOverComponent} from "../game-over/game-over.component";
 import {StatusEnum} from "../enum/status.enum";
-
-interface GameState {
-  status: 'NEW_ROUND' | 'RED_LIGHT' | 'HIT' | 'GAME_OVER';
-  score: number;
-  lives: number;
-  timestamp: Date;
-}
+import { LottieComponent, AnimationOptions } from 'ngx-lottie';
+import { AnimationItem } from 'lottie-web';
 
 @Component({
   selector: 'app-dodge-the-obstacle',
@@ -21,7 +16,8 @@ interface GameState {
     NgForOf,
     NgClass,
     NgIf,
-    GameOverComponent
+    GameOverComponent,
+    LottieComponent
   ],
   templateUrl: './dodge-the-obstacle.component.html',
   styleUrl: './dodge-the-obstacle.component.css'
@@ -31,51 +27,67 @@ export class DodgeTheObstacleComponent implements OnInit,OnDestroy{
   sensorData: any;
   isRunning: boolean = false;
   previousScore: number = 0;
+  private animationItem: any;
+  options: AnimationOptions = {
+    path: 'assets/doraemon-running.json'
+  }
 
-  constructor(private sensorDataService: SensorDataService,private ngZone: NgZone,private audioService: AudioService) {}
+  constructor(
+    private sensorDataService: SensorDataService,
+    private ngZone: NgZone,
+    private audioService: AudioService
+  ) {}
 
   ngOnInit() {
     this.sensorDataService.initDodgeTheObstacle().subscribe({
       next: () => {
         this.sensorSubscription = this.sensorDataService.getSensorData().subscribe(data => {
           this.ngZone.run(() => {
-            console.log(data)
+            console.log(data);
+            const statusChanged = this.sensorData && this.sensorData.status != data.status;
             this.sensorData = data;
-            switch(this.sensorData.status){
-              case StatusEnum.HIT:
-                this.audioService.playError();
-                break;
-              case StatusEnum.GAME_OVER:
-                this.audioService.playGameOver();
-                break;
-              default:
-                break;
+            if (statusChanged) {
+              switch(this.sensorData.status) {
+                case StatusEnum.HIT:
+                  this.audioService.playError();
+                  break;
+                case StatusEnum.GAME_OVER:
+                  this.audioService.playGameOver();
+                  break;
+              }
             }
             this.updateRunningState();
-          })
+          });
         });
-
       }
-    })
+    });
   }
+
   ngOnDestroy() {
-    this.sensorSubscription.unsubscribe();
+    if (this.sensorSubscription) {
+      this.sensorSubscription.unsubscribe();
+    }
   }
 
   updateRunningState() {
-    this.isRunning = this.sensorData.status === StatusEnum.NEW_ROUND && this.sensorData.score && this.sensorData.score > this.previousScore;
+    this.isRunning = this.sensorData.score != undefined && this.sensorData.score > this.previousScore;
     this.previousScore = this.sensorData.score ?? 0;
+  }
+
+  animationCreated(animationItem: AnimationItem): void {
+    this.animationItem = animationItem;
   }
 
   getHeartsFilled(): number[] {
     return Array(this.sensorData?.lives || 0).fill(0);
   }
+
   getHeartsNotFilled(): number[] {
-    return Array(3 - this.sensorData?.lives || 0).fill(0);
+    return Array(3 - (this.sensorData?.lives || 0)).fill(0);
   }
 
   openDialog() {
-
+    // Implementa la logica per aprire il dialogo delle regole
   }
 
   protected readonly StatusEnum = StatusEnum;
